827616a8ff743bd6f29fd05026bfa435fd072000
chulopapi <marlon.guandique@gmail.com>
2020-06-19 15:57:45 -0700

    getting some of the rows and columns

---
+++ Develop/script.js
@@ -0,0 +1,9 @@
+/* This is the javascript program for a Daily Scheduler*
+Written by Marlon Guandique */
+
+
+$("#saveBtn").on('click', function(e) {
+    alert ("I was clicked, going to save")
+    let textinput = $("textArea").val()
+    alert("")
+})
