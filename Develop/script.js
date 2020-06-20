4a39cad279ed5207cecc1b1fb65a458513bbc2a5
chulopapi <marlon.guandique@gmail.com>
2020-06-19 21:14:57 -0700

    adding the time

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,9 @@
 /* This is the javascript program for a Daily Scheduler*
 Written by Marlon Guandique */
 
+//Display the current date
+var dateToday = moment().format(`LLLL`);
+$("#currentDay").append(dateToday);
 
 $("#saveBtn").on('click', function(e) {
     alert ("I was clicked, going to save")
