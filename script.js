ab7f4197924e401f88961b8399fd6d2e264caa4c
chulopapi <marlon.guandique@gmail.com>
2020-06-20 17:22:21 -0700

    local storage and time function

--- script.js
+++ script.js
@@ -1,86 +1,81 @@
-/* This is the javascript program for a Daily Scheduler*
+//console.log("hello world");
+
+/* This is the javascript program for a Work Daily Scheduler*
 Written by Marlon Guandique */
 
 //Display the current date
-var dateToday = moment().format(`LLLL`);
+var dateToday = moment().format(`ddd, ll`);
 $("#currentDay").append(dateToday);
 
-$("#saveBtn").on('btn', function (e) {
-    alert("I was clicked, going to save")
-    let textinput = $("textArea").val()
-    alert("")
-})
+//audit current time
+var auditTask = function() {
+    for (let i = 0; i < 24; i++) {
+        const $div = $("div#" + i);
+        const currentHour = moment().format("H");
+        console.log(currentHour);
 
-// Declares a 'list' variable that holds the parsed to-do items retrieved from 'localStorage'
-// If there is nothing in 'localStorage', sets the 'list' to an empty array
-var list = JSON.parse(localStorage.getItem('description')) || [];
+        if (i > currentHour) {
+            $($div).siblings("textarea")
+            .addClass("future")
+            .removeClass("description");
+        }
+        if (i < currentHour) {
+            $($div).siblings("textarea")
+            .addClass("past")
+            .removeClass("description");
+        }
 
-// Renders our to-dos to the page
-function renderTodos(list) {
-    // Empties out the html
-    $('#to-dos').empty();
+        if (i == currentHour) {
+            $($div).siblings("textarea")
+            .addClass("present")
+            .removeClass("description");
+        }
+        else {
+        }
+    };
+};
+auditTask();
 
-    // Iterates over the 'list'
-    for (var i = 0; i < list.length; i++) {
-        // Creates a new variable 'toDoItem' that will hold a "<p>" tag
-        // Sets the `list` item's value as text of this <p> element
-        var toDoItem = $('<p>');
-        toDoItem.text(list[i]);
+//audit function to run every 30 min
+setInterval(function () {
+    auditTask();
+}, 1800000);
 
-        // Creates a button `toDoClose` with an attribute called `data-to-do` and a unique `id`
-        var toDoClose = $('<button>');
-        toDoClose.attr('data-to-do', i);
+var buttonClicked =$(event.target).attr('data-name');
+var taskTime = buttonClicked;
+var taskDescription = $("#text-area-" + buttonClicked).val().trim();
+console.log(taskTime)
+console.log(taskDescription);
 
-        // Gives the button a class called 'checkbox'
-        toDoClose.addClass('checkbox');
+var task = {
+    id: taskTime,
+    taskDescription: taskDescription
+};
 
-        // Adds a checkmark symbol as its text value
-        toDoClose.text('✓');
+saveTasks(task);
 
-        // Adds the button to the `toDoItem`
-        toDoItem = toDoItem.prepend(toDoClose);
 
-        // Adds 'toDoItem' to the To-Do List div
-        $('#to-dos').append(toDoItem);
+//value text is changed
+
+$("textarea").on("change", "input[type='text']", function () {
+});
+
+//save tasks in jQuery
+$(".saveBtn").click(function (event) {
+});
+
+//helpers functions
+
+function saveTasks(task) {
+    var tasks =JSON.parse(localStorage.getItem("tasks"));
+    if (!tasks) {
+        tasks = [];
     }
+
+    tasks.push(task);
+
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+
+    saveTasks();
+
 }
-
-$('#add-to-do').on('btn', function (event) {
-    event.preventDefault();
-
-    // Get the to-do "value" from the textbox and store it as a variable using `.val()` and `.trim()`
-    var toDoTask = $('#to-do')
-        .val()
-        .trim();
-
-    // Add the new to-do to our local 'list' variable
-    list.push(toDoTask);
-
-    // Update the to-dos on the page
-    renderTodos(list);
-
-    // Save the to-dos into localStorage
-    // We need to use JSON.stringify to turn the list from an array into a string
-    localStorage.setItem('description', JSON.stringify(list));
-
-    // Clear the textbox when done using `.val()`
-    $('#to-do').val('');
-});
-
-$(document).on('btn', '.checkbox', function () {
-    // Get the `id` of the button from its data attribute and hold in a variable called 'toDoNumber'
-    var toDoNumber = $(this).attr('data-to-do');
-
-    // Delete the to-do with that `id` from our local `list` using `.splice()`
-    list.splice(toDoNumber, 1);
-
-    // Update the to-dos on the page
-    renderTodos(list);
-
-    // Save the to-dos into localStorage
-    // We need to use JSON.stringify to turn the list from an array into a string
-    localStorage.setItem('description', JSON.stringify(list));
-});
-
-// render our to-dos on page load
-renderTodos(list);
